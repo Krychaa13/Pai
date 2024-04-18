abf856845fc3e7ff3ab2c9bc2ad4659094ae6977
Krychaa13 <113896168+Krychaa13@users.noreply.github.com>
2024-04-18 16:14:47 +0200

    Add files via upload

---
+++ src/container/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.scss'
+
+const Footer = (props) => {
+    return(
+      <div className = "App-Footer">
+        <h3>App made by Oliwia Krych</h3>
+      </div>
+    );
+}
+export default Footer;
---
+++ src/container/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = (props) => {
+  return (
+    <div className="App-header">
+      <h1>Watter calculator</h1>
+    </div>
+  );
+};
+export default Header;
